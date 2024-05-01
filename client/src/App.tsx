4bd0108199b31415e7f39050c0a045a22fdb7905
Your Name <you@example.com>
2024-05-01 15:11:34 +0300

    add: login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./App.css";
+import "@shoelace-style/shoelace/dist/themes/light.css";
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
 
 import Authorization from "./pages/Auth";
+
+setBasePath(
+  "https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.0/cdn/"
+);
 
 function App() {
   return (
--- client/src/pages/Auth.tsx
+++ client/src/pages/Auth.tsx
@@ -1,7 +1,66 @@
 import React from "react";
 
+import SlIcon from "@shoelace-style/shoelace/dist/react/icon";
+import SlInput from "@shoelace-style/shoelace/dist/react/input";
+
 function Authorization() {
-  return <div className="h-screen w-screen bg-red-300"></div>;
+  return (
+    <div className="h-screen w-screen authorization-wrapper">
+      <div
+        className="w-full flex flex-row justify-between items-center"
+        style={{
+          height: "15%",
+          paddingLeft: "7%",
+          paddingRight: "7%",
+          paddingTop: "2%",
+          paddingBottom: "2%",
+        }}
+      >
+        <div className="w-0">
+          <p className="text-lg">En</p>
+        </div>
+
+        <img
+          style={{ height: "110px" }}
+          src={
+            "https://s3-alpha-sig.figma.com/img/ea58/f75b/3e9abb17fc2a5ae567f590ec48b9b525?Expires=1715558400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=G8HWuqXZe2C1BusOHzy5yu2ArfLXU2ViTrvHhmf-TZOQ223L1rbgLFNrHGINiikuG4OjIzoroR3lDhaTdNQMOSbr4~Ifln5~tkmqeZ7F9GurzpAwKvuQjiwJZzmFcrymKoSPJW9RQxZv2Cf5jcZnnNBpkK-jN4j86oWkTCDJFntUauS1sT9nXUuSOGWNcErYht1PdTm6wq~QMdcVDYB87GYOnYiMcV-n-lsE4~rA0kMcS5nhOsCIBdmyabpY9rhlbbFCABEw1y0zFBKfGPsD00n62~3jkV0mty3DmVdwuqfm6Be4Biyyk8DWvLgFaTLaQEWcZXuqyk9~bFo9Ipo~Vw__"
+          }
+        />
+        <div className="w-0">
+          <p className="text-lg">Logbook</p>
+        </div>
+      </div>
+      <div
+        style={{
+          paddingTop: "7%",
+        }}
+        className="w-full flex justify-center"
+      >
+        <div className="w-1/6">
+          <SlInput placeholder="Username" size="medium" pill>
+            <SlIcon name="person" slot="prefix"></SlIcon>
+          </SlInput>
+          <SlInput
+            className="mt-2"
+            placeholder="Password"
+            size="medium"
+            pill
+            passwordToggle
+          >
+            <SlIcon name="lock" slot="prefix"></SlIcon>
+          </SlInput>
+
+          <div className="select-none mt-8 cursor-pointer w-full bg-blue-600 hover:bg-blue-500 text-center text-white font-bold py-2 rounded-lg">
+            Get Started
+          </div>
+          <div className="w-full flex flex-row justify-between items-center mt-2 select-none">
+            <p className="cursor-pointer">Create Account</p>
+            <p className="cursor-pointer">Need Help?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Authorization;
